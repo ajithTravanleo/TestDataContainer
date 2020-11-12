336f803abf44e9f2a316e1b739195a5aec997e31
ajithTravanleo <ajith.j@travanleo.com>
2020-11-12 15:46:39 +0530

    port update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const cors=require('cors')
-const PORT=process.env.port||3402
+const PORT=process.env.port||3401
 const Utils=require('./utils/utils')
 const app=express()
 const appRouter=require('./routes/appRoute')
